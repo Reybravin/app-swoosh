08496584b8a5538b4b3e96c6b29e0088af362e75
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-10-30 11:05:00 +0200

    Ibactions and model

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Sergiy Sachuk on 10/30/17.
+//  Copyright © 2017 Serhii. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
